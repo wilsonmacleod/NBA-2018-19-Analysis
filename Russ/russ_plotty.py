853b6844ef65e10c6d370a5cd10d8812611a128a
Wilson MacLeod <wilsonmacleod@gmail.com>
2019-04-16 14:15:34 -0700

    more russ

--- Russ/russ_plotty.py
+++ Russ/russ_plotty.py
@@ -6,10 +6,10 @@ import seaborn as sns
 df = pd.read_csv(r'C:\Users\wilso\Desktop\NBA_Analysis\Russ\data\russ18_19.csv')
 
 sns.set_style('whitegrid')
-plt.rcParams.update({'font.size': 14})
+plt.rcParams.update({'font.size': 12})
 
 #3PA vs. Point Diff Figure
-x = sns.jointplot(x=df['Unnamed: 6'], y=df['3PA'], kind='reg', xlim=(-30,30), ylim=(0,15), height = 5.5, 
+x = sns.jointplot(x=df['Unnamed: 6'], y=df['3PA'], kind='reg', xlim=(-30,30), ylim=(0,15), height = 4, 
                 color="#ef6400", marginal_kws={'hist_kws': {'edgecolor': "black"}})
 x.ax_joint.set_xlabel('Point Differential', fontweight='bold')
 x.ax_joint.set_ylabel('3PA', fontweight='bold')
@@ -17,7 +17,7 @@ x.ax_joint.set_ylabel('3PA', fontweight='bold')
 plt.savefig(fname=r'C:\Users\wilso\Desktop\NBA_Analysis\Russ\figs\3pa_pointdiff')
 
 #AST vs. Point Diff Figure
-x = sns.jointplot(x=df['Unnamed: 6'], y=df['AST'], kind='reg', xlim=(-25,30), ylim=(4,22), height = 5.5, 
+x = sns.jointplot(x=df['Unnamed: 6'], y=df['AST'], kind='reg', xlim=(-25,30), ylim=(4,22), height = 4, 
                 color="#408ded", marginal_kws={'hist_kws': {'edgecolor': "black"}})
 x.ax_joint.set_xlabel('Point Differential', fontweight='bold')
 x.ax_joint.set_ylabel('ASTs', fontweight='bold')
